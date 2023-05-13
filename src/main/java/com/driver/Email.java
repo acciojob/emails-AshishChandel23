ce4ee522e9deea7dcbfca8d93c0473cafbcd6161
Ashish Chandel <ashishchandel023@gmail.com>
2023-05-13 15:39:39 +0530

    Initial Done

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -18,6 +18,14 @@ public class Email {
         return password;
     }
 
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     public void changePassword(String oldPassword, String newPassword){
         //Change password only if the oldPassword is equal to current password and the new password meets all of the following:
         // 1. It contains at least 8 characters
@@ -25,5 +33,98 @@ public class Email {
         // 3. It contains at least one lowercase letter
         // 4. It contains at least one digit
         // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
+        if(oldPassword.equals(this.password) && isValid(newPassword)){
+            this.password = newPassword;
+        }
+    }
+    private boolean isValid(String password)
+    {
+
+        // for checking if password length
+        // is between 8 and 15
+        if (!(password.length() >= 8)) {
+            return false;
+        }
+
+        // to check space
+        if (password.contains(" ")) {
+            return false;
+        }
+        if (true) {
+            int count = 0;
+
+            // check digits from 0 to 9
+            for (int i = 0; i <= 9; i++) {
+
+                // to convert int to string
+                String str1 = Integer.toString(i);
+
+                if (password.contains(str1)) {
+                    count = 1;
+                    break;
+                }
+            }
+            if (count == 0) {
+                return false;
+            }
+        }
+
+        // for special characters
+        if (!(password.contains("@") || password.contains("#")
+                || password.contains("!") || password.contains("~")
+                || password.contains("$") || password.contains("%")
+                || password.contains("^") || password.contains("&")
+                || password.contains("*") || password.contains("(")
+                || password.contains(")") || password.contains("-")
+                || password.contains("+") || password.contains("/")
+                || password.contains(":") || password.contains(".")
+                || password.contains(", ") || password.contains("<")
+                || password.contains(">") || password.contains("?")
+                || password.contains("|"))) {
+            return false;
+        }
+
+        if (true) {
+            int count = 0;
+
+            // checking capital letters
+            for (int i = 65; i <= 90; i++) {
+
+                // type casting
+                char c = (char)i;
+
+                String str1 = Character.toString(c);
+                if (password.contains(str1)) {
+                    count = 1;
+                    break;
+                }
+            }
+            if (count == 0) {
+                return false;
+            }
+        }
+
+        if (true) {
+            int count = 0;
+
+            // checking small letters
+            for (int i = 97; i <= 122; i++) {
+
+                // type casting
+                char c = (char)i;
+                String str1 = Character.toString(c);
+
+                if (password.contains(str1)) {
+                    count = 1;
+                    break;
+                }
+            }
+            if (count == 0) {
+                return false;
+            }
+        }
+
+        // if all conditions fails
+        return true;
     }
 }
--- src/main/java/com/driver/Workspace.java
+++ src/main/java/com/driver/Workspace.java
@@ -4,6 +4,7 @@ import org.apache.commons.lang3.tuple.Pair;
 
 import java.time.LocalTime;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 
 public class Workspace extends Gmail{
@@ -12,12 +13,13 @@ public class Workspace extends Gmail{
 
     public Workspace(String emailId) {
         // The inboxCapacity is equal to the maximum value an integer can store.
-
+        super(emailId,Integer.MAX_VALUE);
+        this.calendar = new ArrayList<>();
     }
 
     public void addMeeting(Meeting meeting){
         //add the meeting to calendar
-
+        calendar.add(meeting);
     }
 
     public int findMaxMeetings(){
@@ -25,6 +27,30 @@ public class Workspace extends Gmail{
         // 1. At a particular time, you can be present in at most one meeting
         // 2. If you want to attend a meeting, you must join it at its start time and leave at end time.
         // Example: If a meeting ends at 10:00 am, you cannot attend another meeting starting at 10:00 am
-
+        int countMeetings = 0;
+        Collections.sort(calendar, (a,b)-> (a.getStartTime().isBefore(b.getStartTime()))
+                        ? -1
+                        : (a.getStartTime().isAfter(b.getStartTime()) ? 1 : 0));
+//        for(Meeting m : calendar) {
+//            System.out.print(m.getStartTime() +" "+ m.getEndTime()+" ");
+//        }
+        if(calendar.isEmpty()) return countMeetings;
+        countMeetings=1;
+        LocalTime prevEndTime = calendar.get(0).getEndTime();
+        for(int i=1;i<calendar.size();i++){
+            if(calendar.get(i).getStartTime().isBefore(prevEndTime)){
+                continue;
+            }
+            else if(!calendar.get(i).getStartTime().isBefore(prevEndTime) &&
+                    !calendar.get(i).getStartTime().isAfter(prevEndTime)){
+                continue;
+            }
+            else {
+                countMeetings++;
+                prevEndTime = calendar.get(i).getEndTime();
+            }
+        }
+        System.out.println();
+        return countMeetings;
     }
 }
